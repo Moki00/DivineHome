af48a19e601f868ca69ad1733d200953097eded1
MoKi <Mokiwins@gmail.com>
2025-01-25 23:05:50 -0500

    add scripts

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Email form
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    // init("USER_ID");
+    emailjs.init("k_mqvfpXwVIJ2nuSw");
+
+    // Collect form data
+    const formData = {
+      name: this.querySelector("[name='from_name']").value,
+      phone: this.querySelector("[name='phone_number']").value,
+      email: this.querySelector("[name='email']").value,
+      message: this.querySelector("[name='message']").value,
+    };
+
+    // emailjs:SERVICE_ID-TEMPLATE_ID-formData
+    emailjs
+      .send("service_owu350n", "template_vna2gyi", formData)
+      .then(() => {
+        alert("Thank you for your message! We will get back to you soon.");
+        this.reset();
+      })
+      .catch((error) => {
+        console.error("EmailJS error:", error);
+        alert("Error sending your message. Please try again later.");
+      });
+  });
